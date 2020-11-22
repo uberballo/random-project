b30d6299c96e325b2accbda7882031a98e47680d
uberballo <kasper.paavolainen@hotmail.com>
2020-11-22 19:50:46 +0200

    Add some .css

--- front/src/components/Project.js
+++ front/src/components/Project.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import '../styles/person.css'
+import '../styles/project.css'
+
+
 
 const Project = ({ project, removeProject }) => {
   return (
-    <div className="project">
+    <div className='project-item'>
       <p className="projectTitle">{project.title}</p>
-      <p className="projectBody">{project.body}</p>
       <button onClick={() => removeProject(project.id)}>X</button>
     </div>
   )
--- front/src/components/ProjectForm.js
+++ front/src/components/ProjectForm.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-const ProjectForm= ({title, body, handleSubmit}) => {
+const ProjectForm = ({ title, body, handleSubmit }) => {
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label>Title:</label>
-        <input placeholder={'Enter project name here!'} {...title} />
+        <input placeholder={'Enter project name here!'} value={title.value} onChange={title.onChange} />
         <label>Body:</label>
-        <input placeholder={'Enter project description here'} {...body} />
+        <input placeholder={'Enter project description here'} value={body.value} onChange={body.onChange} />
       </div>
       <button type="submit">Submit</button>
     </form>
